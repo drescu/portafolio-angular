066234bf43291ceb50803f09bc1eb3ff1eba0b78
Diego Escudero <drescu@gmail.com>
2019-07-01 00:05:37 -0300

    fin seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,22 +9,64 @@ export class ProductosService {
 
    cargando = true;
    productos: Producto[] = []; 
+   productosFiltrado: Producto[] = []; 
 
-  constructor( private http: HttpClient ) {
+  constructor( private http: HttpClient ) { 
     
     this.cargarProductos();
    
   }
-
+  
   private cargarProductos() {
 
-    this.http.get('https://angular-html-40138.firebaseio.com/productos_idx.json')
-         .subscribe( (resp: Producto[]) => {
-         
-            console.log(resp);
-            this.productos = resp; 
-            this.cargando = false; 
+    return new Promise( ( resolve, reject ) => {
 
-         });
+      this.http.get('https://angular-html-40138.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[]) => {
+         this.productos = resp; 
+         this.cargando = false; 
+         resolve();
+      });
+    }); 
+  }
+
+  getProducto( id: string ) {
+
+    return this.http.get(`https://angular-html-40138.firebaseio.com/productos/${ id }.json`); 
+
+  }
+
+  buscarProducto( termino: string ) {
+
+    if( this.productos.length === 0 ) {
+        //cargar productos
+        this.cargarProductos().then( () => {
+            //despues de tener los productos
+            //aplicar filtro
+            this.filtrarProductos( termino );
+        });
+    } else {
+        //aplicar filtro
+        this.filtrarProductos( termino );
+    }
+  }
+
+  private filtrarProductos( termino: string ) {
+
+      //console.log(this.productos);
+      this.productosFiltrado = [];
+
+      termino = termino.toLocaleLowerCase();
+
+      this.productos.forEach( prod => {
+
+          const tituloLower = prod.titulo.toLocaleLowerCase();
+
+          if( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >=0 ) {
+              this.productosFiltrado.push( prod );
+          }
+
+      });
+    
   }
 }
